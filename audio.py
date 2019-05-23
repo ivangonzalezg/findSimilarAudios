fd80d697f71476dfaacd578a7ba67552c66db519
Ivan Gonzalez <ivandario1998@gmail.com>
2019-05-22 19:49:58 -0500

    Styling output

--- audio.py
+++ audio.py
@@ -1,44 +1,50 @@
-from glob import glob
+import sys
 import numpy as np
 import librosa as lr
+from glob import glob
 
-#Audio a comparar
+# Audio to compare
 
 userAudioDir = "./userAudio.wav"
 userAudio, userFrequency = lr.load(userAudioDir)
 
-#Comparador
+# Comparator
 
 audiosDir = "./originalAudios"
 audioFiles = glob(audiosDir + "/*.wav")
 maxCorrelation = 0
 maxName = ""
+maxType = ""
+counter = 1
 for x in range(len(audioFiles)):
     testAudio, testFrequency = lr.load(audioFiles[x])
     lenDifference = len(userAudio) - len(testAudio)
-    print(lenDifference)
     if lenDifference > 0:
-        print("User audio is bigger")
         correlation = np.corrcoef(userAudio[0:len(testAudio)], testAudio)
         if correlation[1][0] > maxCorrelation:
             maxCorrelation = correlation[1][0]
-            maxName = audioFiles[x].split("/")[2].split(".")[0].split("_")[0]
+            maxName = audioFiles[x].split("/")[2].split(".")[0]
     elif lenDifference < 0:
-        print("User test is bigger")
         correlation = np.corrcoef(testAudio[0:len(userAudio)], userAudio)
         if correlation[1][0] > maxCorrelation:
             maxCorrelation = correlation[1][0]
-            maxName = audioFiles[x].split("/")[2].split(".")[0].split("_")[0]
+            maxName = audioFiles[x].split("/")[2].split(".")[0]
     else:
-        print("Equal len")
         correlation = np.corrcoef(userAudio, testAudio)
-        print(correlation[1][0])
         if correlation[1][0] > maxCorrelation:
             maxCorrelation = correlation[1][0]
-            maxName = audioFiles[x].split("/")[2].split(".")[0].split("_")[0]
-    print("------------------------------------")
+            maxName = audioFiles[x].split("/")[2].split(".")[0]
+    sys.stdout.write("\r" + "Buscando" + "." * counter)
+    counter += 1
+    if counter > 3:
+        counter = 1
 
-    #print(len(testAudio))
-print("Done!")
-print(maxCorrelation)
-print(maxName)
+sys.stdout.write("\n")
+if maxName.split("_")[0] == "ECG":
+    maxType = "Señal Electrocardiográfica"
+elif maxName.split("_")[0] == "EMG":
+    maxType = "Señal Electromiográfica"
+elif maxName.split("_")[0] == "EEG":
+    maxType = "Señal Electroencefalográfica"
+
+print("Tipo: " + maxType)
