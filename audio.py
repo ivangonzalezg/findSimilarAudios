ba6b3fbac4fde2b4c2ab55791818d828633b2bfa
Ivan Gonzalez <ivandario1998@gmail.com>
2019-05-22 19:21:28 -0500

    Everything has a price

--- audio.py
+++ audio.py
@@ -19,16 +19,18 @@ for x in range(len(audioFiles)):
     print(lenDifference)
     if lenDifference > 0:
         print("User audio is bigger")
-        correlation = np.corrcoef(userAudio[0:len(testAudio)], testAudio)
-        if correlation[1][0] > maxCorrelation:
-            maxCorrelation = correlation[1][0]
-            maxName = audioFiles[x].split("/")[2].split(".")[0].split("_")[0]
+        for y in range(lenDifference):
+            correlation = np.corrcoef(userAudio[y:len(testAudio)+y], testAudio)
+            if correlation[1][0] > maxCorrelation:
+                maxCorrelation = correlation[1][0]
+                maxName = audioFiles[x].split("/")[2].split(".")[0].split("_")[0]
     elif lenDifference < 0:
         print("User test is bigger")
-        correlation = np.corrcoef(testAudio[0:len(userAudio)], userAudio)
-        if correlation[1][0] > maxCorrelation:
-            maxCorrelation = correlation[1][0]
-            maxName = audioFiles[x].split("/")[2].split(".")[0].split("_")[0]
+        for y in range(lenDifference):
+            correlation = np.corrcoef(testAudio[y:len(userAudio)+y], userAudio)
+            if correlation[1][0] > maxCorrelation:
+                maxCorrelation = correlation[1][0]
+                maxName = audioFiles[x].split("/")[2].split(".")[0].split("_")[0]
     else:
         print("Equal len")
         correlation = np.corrcoef(userAudio, testAudio)
